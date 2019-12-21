c68c8373fa43aa0be9735897939775c00d7bbf59
chenhonghe <honghe.chen.ch@partner.bmw-brilliance.cn>
2019-12-21 21:11:12 +0800

    实现

--- src/main/java/cn/xp/dojo/Calculator.java
+++ src/main/java/cn/xp/dojo/Calculator.java
@@ -3,6 +3,6 @@ package cn.xp.dojo;
 public class Calculator {
 
     public int add(int x, int y) {
-        throw new UnsupportedOperationException("not implemented yet");
+        return x + y;
     }
 }
